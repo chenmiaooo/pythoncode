4286fa22922237940e79e5d06a74afaea88e1db0
chenmiaooo <robin134cm@163.com>
2018-01-17 12:31:37 +0800

    first commit

--- test.py
+++ test.py
@@ -5,3 +5,11 @@ blue = 0,0,200
 pygame.init()
 screen = pygame.display.set_mode((600,500))
 myfont = pygame.font.Font(None,60)
+textImage = myfont.render("Hello Pygame",True,white)
+while True:
+    for event in pygame.event.get():
+        if event.type in (QUIT,KEYDOWN):
+            exit(0)
+        screen.fill(blue)
+        screen.blit(textImage,(100,100))
+        pygame.display.update()
